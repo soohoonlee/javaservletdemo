ba723127ea2006d99b38ee94de9f90ce06bbbac7
soohoonlee <jjinimania@gmail.com>
2019-03-29 01:29:53 +0900

    9. Implements WebMvcConfigurer

--- src/main/java/me/ssoon/WebConfig.java
+++ src/main/java/me/ssoon/WebConfig.java
@@ -1,22 +1,18 @@
 package me.ssoon;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-  @Bean
-  public ViewResolver viewResolver() {
-    final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-    viewResolver.setPrefix("/WEB-INF/");
-    viewResolver.setSuffix(".jsp");
-    return viewResolver;
-  }
+    @Override
+    public void configureViewResolvers(final ViewResolverRegistry registry) {
+        registry.jsp();
+    }
 }
