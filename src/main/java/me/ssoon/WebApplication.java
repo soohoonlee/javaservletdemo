6d0f7bb29c38f4a9ce208e09fe7ff8470bbaf96e
soohoonlee <jjinimania@gmail.com>
2019-03-28 23:07:21 +0900

    7. Without web.xml

---
+++ src/main/java/me/ssoon/WebApplication.java
@@ -0,0 +1,22 @@
+package me.ssoon;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebApplication implements WebApplicationInitializer {
+
+  @Override
+  public void onStartup(final ServletContext servletContext) throws ServletException {
+    final AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+    context.register(WebConfig.class);
+    context.refresh();
+
+    final DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+    final Dynamic app = servletContext.addServlet("app", dispatcherServlet);
+    app.addMapping("/app/*");
+  }
+}
